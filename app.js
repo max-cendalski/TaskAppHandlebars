e77f28c5b27a290660913a838c2c8551d5d9bc32
Korneliu <olbiton@gmail.com>
2020-05-08 15:17:01 -0700

    added Process Form, Get Tasks endpoints

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ const port = 3000
 
 // Handlebars
 app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+
 app.engine('handlebars', exphbs({
     handlebars: allowInsecurePrototypeAccess(Handlebars),
     defaultLayout: 'main'
 }));
+
+app.set('view engine', 'handlebars');
+// Load Task Model
+require('./models/task')
+const Task = mongoose.model('Tasks')
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,12 +31,66 @@ mongoose.connect('mongodb://localhost/TaskAppHandlebars', {
     useCreateIndex: true
 })
 
-// Main page
 
+
+// Main page
 app.get('/', (req, res) => {
-    const title = 'Welcome'
-    res.render('index', { title })
+        const title = 'Welcome'
+        res.render('index', { title })
+    })
+    // About page
+app.get('/about', (req, res) => {
+    res.render('about')
 })
+
+// Add Task Form
+app.get('/tasks/add', (req, res) => {
+    res.render('tasks/add')
+})
+
+// Get Tasks
+app.get('/tasks', (req, res) => {
+    Task.find({})
+        .sort({ date: 'desc' })
+        .then(ideas => {
+            res.render('tasks/index', {
+                ideas: ideas
+            })
+        })
+})
+
+
+
+
+// Process Form
+app.post('/tasks', (req, res) => {
+    let errors = []
+
+    if (!req.body.title) {
+        errors.push({ text: 'Please add title' })
+    }
+    if (!req.body.details) {
+        errors.push({ text: 'Please add details' })
+    }
+    if (errors.length > 0) {
+        res.render('tasks/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details,
+        })
+    } else {
+        const newTask = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Task(newTask)
+            .save()
+            .then(task => {
+                res.redirect('/tasks')
+            })
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`App listen to port ${port}`)
---
+++ models/task.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TaskSchema = new Schema({
+    title: {
+        required: true,
+        type: String
+
+    },
+    details: {
+        required: true,
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+mongoose.model('Tasks', TaskSchema)
